56a8985b2dd86252937caeb4856ac576bdb8ae59
dylcaton <dylanpcaton@gmail.com>
2026-03-24 23:24:21 -0500

    up to vid 27

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
@@ -1,12 +1,12 @@
 package edu.tcu.cs.hogwartsartifactsonline.artifact;
 
+import edu.tcu.cs.hogwartsartifactsonline.artifact.converter.ArtifactDtoToArtifactConverter;
 import edu.tcu.cs.hogwartsartifactsonline.artifact.converter.ArtifactToArtifactDtoConverter;
 import edu.tcu.cs.hogwartsartifactsonline.artifact.dto.ArtifactDto;
 import edu.tcu.cs.hogwartsartifactsonline.system.Result;
 import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,14 +14,19 @@ import java.util.stream.Collectors;
 @RestController
 public class ArtifactController {
 
+
+
     private final ArtifactService artifactService;
 
-    public ArtifactController(ArtifactService artifactService, ArtifactToArtifactDtoConverter artifactToArtifactDtoConverter) {
+    private final ArtifactToArtifactDtoConverter artifactToArtifactDtoConverter;
+
+    private final ArtifactDtoToArtifactConverter artifactDtoToArtifactConverter;
+
+    public ArtifactController(ArtifactService artifactService, ArtifactToArtifactDtoConverter artifactToArtifactDtoConverter, ArtifactDtoToArtifactConverter artifactDtoToArtifactConverter) {
         this.artifactService = artifactService;
         this.artifactToArtifactDtoConverter = artifactToArtifactDtoConverter;
+        this.artifactDtoToArtifactConverter = artifactDtoToArtifactConverter;
     }
-
-    private final ArtifactToArtifactDtoConverter artifactToArtifactDtoConverter;
 
 
     @GetMapping("/api/v1/artifacts/{artifactId}")
@@ -37,5 +42,28 @@ public class ArtifactController {
                 .map(this.artifactToArtifactDtoConverter::convert)
                 .collect(Collectors.toList());
         return new Result(true, StatusCode.SUCCESS, "Find All Success", artifactDtos);
+    }
+
+    @PostMapping("/api/v1/artifacts")
+    public Result addArtifact(@Valid @RequestBody ArtifactDto artifactDto) {
+        //convert artifactdto to artifact
+        Artifact newArtifact = this.artifactDtoToArtifactConverter.convert(artifactDto);
+        Artifact savedArtifact = this.artifactService.save(newArtifact);
+        ArtifactDto savedArtifactDto = this.artifactToArtifactDtoConverter.convert(savedArtifact);
+        return new Result(true, StatusCode.SUCCESS, "Add Success", savedArtifactDto);
+    }
+
+    @PutMapping("/api/v1/artifacts/{artifactId}")
+    public Result updateArtifact(@PathVariable String artifactId, @Valid @RequestBody ArtifactDto artifactDto) {
+        Artifact update = this.artifactDtoToArtifactConverter.convert(artifactDto);
+        Artifact updatedArtifact = this.artifactService.update(artifactId, update);
+        ArtifactDto updatedArtifactDto = this.artifactToArtifactDtoConverter.convert(updatedArtifact);
+        return new Result(true, StatusCode.SUCCESS, "Update Success", updatedArtifactDto);
+    }
+
+    @DeleteMapping("/api/v1/artifacts/{artifactId}")
+    public Result deleteArtifact(@PathVariable String artifactId) {
+        this.artifactService.delete(artifactId);
+        return new Result(true, StatusCode.SUCCESS, "Delete Success");
     }
 }
